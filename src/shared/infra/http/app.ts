9f6a12f57a36cbcc2c0f5e3e39db1c70f78409ef
Rafael Souza <rafasouza2509@gmail.com>
2021-09-12 23:54:51 +0000

    configured sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,8 @@ import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import cors from 'cors';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 import AppError from '@shared/errors/AppError';
 
@@ -21,9 +23,20 @@ createConnection();
 
 const app = express();
 
-app.use(express.json());
-
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+app.use(express.json());
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(doc));
 app.use('/avatar', express.static(`${uploadConfig.tmpFolder}/avatar`));
@@ -31,6 +44,8 @@ app.use('/cars', express.static(`${uploadConfig.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(appRoutes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 /* eslint-disable no-console */
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
